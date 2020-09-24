8bfb14d1b0a19056cfd8d7237090f293f84408f6
Genievot <40867747+genievot@users.noreply.github.com>
2020-09-24 20:09:29 +0530

    arguments...

--- app/contracts/interfaces/randomness_interface.sol
+++ app/contracts/interfaces/randomness_interface.sol
@@ -3,5 +3,5 @@ pragma solidity 0.6.6;
 interface randomness_interface {
     function randomNumber(uint256) external view returns (uint256);
 
-    function getRandom(string) external;
+    function getRandom(string calldata) external;
 }
